40ecaddbcac7ca38ef557665efd338c29bcbcc25
Andrei Marinica <andrei.marinica@elrond.com>
2022-10-21 13:26:46 +0300

    cleanup

--- exec-c-api/src/capi_signals.rs
+++
@@ -1,8 +0,0 @@
-use crate::service_singleton::with_service;
-
-#[no_mangle]
-pub unsafe extern "C" fn vm_exec_set_sigsegv_passthrough() {
-    with_service(|service| {
-        service.update_last_error_str("Rust: wasmer_set_sigsegv_passthrough called!".to_string());
-    });
-}
