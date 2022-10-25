fecedfea32a1d5077d5e79e18ae00beb2a84c1c6
Ovidiu Stinga <ovstinga@gmail.com>
2022-10-25 13:12:58 +0300

    change bridge names

--- exec-c-api/src/capi_metering.rs
+++ exec-c-api/src/capi_metering.rs
@@ -9,16 +9,22 @@ pub static mut OPCODE_COSTS: [u32; OPCODE_COUNT] = [0; OPCODE_COUNT];
 // TODO: add comments
 #[allow(clippy::cast_ptr_alignment)]
 #[no_mangle]
-pub unsafe extern "C" fn wasmer_set_opcode_costs(opcode_costs_pointer: *const u32) {
+pub unsafe extern "C" fn vm_exec_set_opcode_costs(opcode_costs_pointer: *const u32) {
+    println!("vm_exec_set_opcode_costs");
     OPCODE_COSTS.copy_from_slice(slice::from_raw_parts(opcode_costs_pointer, OPCODE_COUNT));
+    for (i, cost) in OPCODE_COSTS.iter().enumerate() {
+        println!("opcode {} cost {}", i, cost);
+    }
+    println!();
 }
 
 // TODO: add comments
 #[allow(clippy::cast_ptr_alignment)]
 #[no_mangle]
-pub unsafe extern "C" fn wasmer_instance_get_points_used(
+pub unsafe extern "C" fn vm_exec_instance_get_points_used(
     instance_ptr: *const vm_exec_instance_t,
 ) -> u64 {
+    println!("vm_exec_instance_get_points_used");
     let capi_instance =
         cast_input_const_ptr!(instance_ptr, CapiInstance, "instance ptr is null", 0);
     capi_instance.content.get_points_used()
@@ -27,10 +33,11 @@ pub unsafe extern "C" fn wasmer_instance_get_points_used(
 // TODO: add comments
 #[allow(clippy::cast_ptr_alignment)]
 #[no_mangle]
-pub unsafe extern "C" fn wasmer_instance_set_points_used(
+pub unsafe extern "C" fn vm_exec_instance_set_points_used(
     instance_ptr: *const vm_exec_instance_t,
     new_gas: u64,
 ) {
+    println!("vm_exec_instance_set_points_used");
     if instance_ptr.is_null() {
         with_service(|service| service.update_last_error_str("instance ptr is null".to_string()));
         return;
@@ -42,10 +49,11 @@ pub unsafe extern "C" fn wasmer_instance_set_points_used(
 // TODO: add comments
 #[allow(clippy::cast_ptr_alignment)]
 #[no_mangle]
-pub unsafe extern "C" fn wasmer_instance_set_points_limit(
+pub unsafe extern "C" fn vm_exec_instance_set_points_limit(
     instance_ptr: *mut vm_exec_instance_t,
     new_limit: u64,
 ) {
+    println!("vm_exec_instance_set_points_limit");
     if instance_ptr.is_null() {
         with_service(|service| service.update_last_error_str("instance ptr is null".to_string()));
         return;
