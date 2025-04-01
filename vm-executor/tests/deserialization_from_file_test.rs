4650ab6225c38f232bfefc3ae6d54ae307e6340c
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2025-04-01 20:28:39 +0200

    first implementation + test

---
+++ vm-executor/tests/deserialization_from_file_test.rs
@@ -0,0 +1,20 @@
+use multiversx_chain_vm_executor::OpcodeCost;
+use std::path::Path;
+
+const GAS_SCHEDULE_V8: &str = "gasScheduleV8.toml";
+
+#[test]
+fn deserialization_test_from_file() {
+    let file_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("tests")
+        .join(GAS_SCHEDULE_V8);
+
+    let result = OpcodeCost::from_file(file_path);
+    assert!(result.is_ok(), "Failed to deserialize: {:?}", result.err());
+
+    let opcode_cost = result.unwrap();
+
+    assert_eq!(opcode_cost.opcode_unreachable, 5);
+    assert_eq!(opcode_cost.opcode_nop, 5);
+    assert_eq!(opcode_cost.opcode_block, 5);
+}
