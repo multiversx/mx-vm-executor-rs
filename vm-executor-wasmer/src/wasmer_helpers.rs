a5c00c1dc9a4d4b608a60b5e4dfc580a1067a213
Andrei Marinica <andrei.marinica@multiversx.com>
2026-09-08 16:09:45 +0300

    fix after review

--- vm-executor-wasmer/src/wasmer_helpers.rs
+++ vm-executor-wasmer/src/wasmer_helpers.rs
@@ -86,6 +86,6 @@ pub(crate) fn is_control_flow_operator(operator: &Operator) -> bool {
 pub(crate) fn is_supported_bulk_memory_operator(operator: &Operator) -> bool {
     matches!(
         operator,
-        Operator::MemoryCopy { src: _, dst: _ } | Operator::MemoryFill { mem: _ }
+        Operator::MemoryCopy { .. } | Operator::MemoryFill { .. }
     )
 }
